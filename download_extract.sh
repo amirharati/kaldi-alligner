16f107eb68cad048b4825ee7c3ea2949700a5102
amir <amir.harati@gmail.com>
2018-04-27 16:51:38 -0700

    add download_extract

---
+++ download_extract.sh
@@ -0,0 +1,9 @@
+# download and extract model
+wget http://dl.kaldi-asr.org/models/0001_aspire_chain_model.tar.gz
+tar -xzf 0001_aspire_chain_model.tar.gz
+
+# prepare the model
+steps/online/nnet3/prepare_online_decoding.sh \
+  --mfcc-config conf/mfcc_hires.conf data/lang_chain exp/nnet3/extractor exp/chain/tdnn_7b exp/tdnn_7b_chain_online
+
+utils/mkgraph.sh --self-loop-scale 1.0 data/lang_pp_test exp/tdnn_7b_chain_online exp/tdnn_7b_chain_online/graph_pp

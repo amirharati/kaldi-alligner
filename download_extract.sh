25292491628b3e98721d4232621788d76ec90f13
amir <amir.harati@gmail.com>
2018-04-27 18:10:04 -0700

    add readme.txt and update download_extract.sh

--- download_extract.sh
+++ download_extract.sh
@@ -1,7 +1,11 @@
 # download and extract model
+mkdir temp
+cd temp
 wget http://dl.kaldi-asr.org/models/0001_aspire_chain_model.tar.gz
 tar -xzf 0001_aspire_chain_model.tar.gz
-
+mv exp ..
+mv data ..
+cd ..
 # prepare the model
 steps/online/nnet3/prepare_online_decoding.sh \
   --mfcc-config conf/mfcc_hires.conf data/lang_chain exp/nnet3/extractor exp/chain/tdnn_7b exp/tdnn_7b_chain_online
